28dd9060b2d2665756ef595ab7ab39aa0a187b73
DESKTOP-ITJGVFT\vadim <399115@gmail.com>
2017-11-16 21:39:52 +0200

    Added required module files

---
+++ Module.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Core;
+
+use Zend\Mvc\Controller\ControllerManager;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+	
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig() 
+    {
+        return [
+            'factories' => [];
+        ];
+    }
+
+    public function getControllerConfig() 
+    {
+        return [
+            'factories' => [];
+        ];
+    }
+
+    public function getControllerPluginConfig()
+    {
+        return [
+            'factories'     => [],
+            'invokables'    => [],
+        ];
+    }
+
+    public function getViewHelperConfig()
+    {
+        return [
+            'factories'     => [],
+        ];  
+    }
+}
